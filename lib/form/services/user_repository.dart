d88426aef69a7d40b34f76f0b2543dc36847b095
kbrac <kktkate28@gmail.com>
2023-02-05 01:58:41 +0800

    add update form bloc

--- lib/form/services/user_repository.dart
+++ lib/form/services/user_repository.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import '../../Authentication/model/usermodel.dart';
+import '../../form/model/usermodel.dart';
 
 class UserRepository {
   final FirebaseAuth _firebaseAuth;
